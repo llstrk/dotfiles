7ee19379092a87e7e1e2a0ef7c5cc91c09e86a46
Dennis Rye <dryerr@gmail.com>
2024-01-23 12:04:05 +0100

    Initial commit

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,34 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+    },
+    lazy = false,
+    config = function()
+        -- should be moved to keymaps
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") });
+        end)
+
+        local actions = require("telescope.actions")
+
+        require("telescope").setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-q>"] = actions.send_selected_to_qflist,
+                    }
+                }
+            }
+        })
+
+        require("telescope").load_extension("fzf")
+    end
+}
