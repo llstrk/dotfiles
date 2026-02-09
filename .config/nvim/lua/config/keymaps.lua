2f062c2c042711790ce500ee02de0f00a6c4fe90
Dennis Rye <dryerr@gmail.com>
2026-02-09 23:41:52 +0100

    Add current nvim

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<ESC>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("v", "<leader>y", '"+y')
+
+vim.keymap.set("n", "<BS>", "<C-w>h")
+
+-- Disable LazyVim Copilot plugin autocomplete
+-- vim.g.copilot_no_tab_map = true
+-- vim.keymap.set("i", "<S-Tab>", 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
+--
+-- vim.keymap.set("n", "<leader>xc", ":CopilotChatToggle<CR>")
