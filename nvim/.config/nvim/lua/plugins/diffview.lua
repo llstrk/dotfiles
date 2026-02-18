cbd02e6b253506fc7716e34ef9a629675ffbe946
Dennis Rye <dennis.rye@danfoss.com>
2026-02-18 16:30:26 +0000

    Add hotkeys for diffview

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,3 +1,13 @@
 return {
-  "sindrets/diffview.nvim",
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>gvo", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+      { "<leader>gvc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+      { "<leader>gvh", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview History (Repo)" },
+      { "<leader>gvf", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview History (File)" },
+      { "<leader>gvt", "<cmd>DiffviewToggleFiles<cr>", desc = "Diffview Toggle Files" },
+      { "<leader>gvr", "<cmd>DiffviewRefresh<cr>", desc = "Diffview Refresh" },
+    },
+  },
 }
